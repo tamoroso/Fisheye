7fa4a487c9a92a21509b11d17ad35ee1de0e3b42
tamoroso <amorosothomas@orange.fr>
2022-06-02 11:01:57 +0200

    removing eventListener and replacing by a tag

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -3,25 +3,19 @@ function photographerFactory(data) {
 
   const picture = `assets/photographers/${portrait}`;
 
-  const handleCardClick = (id) => {
-    let currentUrl = new URL(window.location.href);
-    currentUrl.searchParams.append("id", id);
-    currentUrl.pathname = "/photographer.html";
-    window.location.href = currentUrl.href;
-  };
-
   function getUserCardDOM() {
     const article = document.createElement("article");
     article.id = id;
     const articleContent = `
+    <a href=/photographer.html?id=${id}>
     <img src = "${picture}"/>
     <h2>${name}</h2>
     <p class="location">${city}, ${country}</p>
     <p class="tagline">${tagline}</p>
     <p class="price">${price}€/jour</p>
+    </a>
     `;
     article.innerHTML = articleContent;
-    article.addEventListener("click", () => handleCardClick(id));
     return article;
   }
   return { name, picture, getUserCardDOM };
